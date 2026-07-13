9944ff9b8b069de0b7301257ef16615c920ceead
karim.chalabiii@gmail.com <karim.chalabiii@gmail.com>
2026-07-13 21:51:03 +0000

    test: add staging documentation and data quality tests

--- models/staging/stg_order_items.sql
+++ models/staging/stg_order_items.sql
@@ -1,8 +1,9 @@
 select
-    item_id,
-    discount,
+    format('%s-%s', order_id, item_id) as order_item_sk,
     order_id,
+    item_id,
+    product_id,
     quantity,
     list_price,
-    product_id
+    discount
 from {{ source('raw_local_bike', 'order_items') }}
--- models/staging/stg_stocks.sql
+++ models/staging/stg_stocks.sql
@@ -1,5 +1,6 @@
 select
-    quantity,
+    format('%s-%s', store_id, product_id) as stock_sk,
     store_id,
-    product_id
+    product_id,
+    quantity
 from {{ source('raw_local_bike', 'stocks') }}
